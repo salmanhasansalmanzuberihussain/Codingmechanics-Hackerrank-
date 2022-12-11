863eefb17a76c1256815da02794153a36a1e2244
Salman Hasan <salmanhasan@Salmans-MacBook-Pro.local>
2022-12-11 01:10:45 -0500

    First Commit

---
+++ Hackerrank.js
@@ -0,0 +1,58 @@
+//Hackerrank 3 month preperation kit
+
+//Plus Minus
+function plusMinus(array) {
+  let positive = 0;
+  let negative = 0;
+  let zero = 0;
+  let len = array.length;
+
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] > 0) {
+      positive += 1;
+    } else if (array[i] < 0) {
+      negative += 1;
+    } else {
+      zero += 1;
+    }
+  }
+
+  let pos = positive / len;
+  let neg = negative / len;
+  let zer = zero / len;
+
+  console.log(pos.toFixed(6));
+  console.log(neg.toFixed(6));
+  console.log(zer.toFixed(6));
+}
+
+//Mini Max Sum
+function miniMaxSum(arr) {
+  let sum = 0;
+  let arrs = arr.sort();
+
+  for (i = 0; i < arrs.length; i++) {
+    sum += arrs[i];
+  }
+
+  let max = sum - arrs[0];
+  let min = sum - arrs[arrs.length - 1];
+
+  console.log(min, max);
+}
+
+//Time Conversion
+function timeConversion(s) {
+  let late = s.includes('PM');
+  if (!late) {
+    s = s.split('AM');
+    s = s[0].split(':');
+    if (s[0] === '12') s[0] = '00';
+    return s.join(':');
+  } else {
+    s = s.split('PM');
+    s = s[0].split(':');
+    if (s[0] !== '12') s[0] = (parseInt(s[0]) + 12).toString();
+    return s.join(':');
+  }
+}
